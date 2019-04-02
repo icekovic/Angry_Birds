30f9dd911a25acfeac644eeb9cdbdce020e723b0
icekovic <icekovic@racunarstvo.hr>
2019-04-02 16:50:55 +0200

    created message canvas

---
+++ AngryBirds/Assets/Source/Scenes/CanvasMessageManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasMessageManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameCompleted;
+
+    [SerializeField]
+    private GameObject gameOver;
+
+    private void Awake()
+    {
+        gameCompleted.SetActive(false);
+        gameOver.SetActive(false);
+    }
+
+    public void ShowGameCompletedMessage()
+    {
+        gameCompleted.SetActive(true);
+    }
+
+    public void ShowGameOverMessage()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+}
